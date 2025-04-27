baa9325aa9283f4789cdde6cbe2fb524ed1569b8
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2025-04-27 19:49:36 -0300

    Initial Commit

---
+++ TabKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabKeeper
+//
+//  Created by Felipe Ribeiro on 27/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabKeeper/TabKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabKeeperApp.swift
+//  TabKeeper
+//
+//  Created by Felipe Ribeiro on 27/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TabKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
